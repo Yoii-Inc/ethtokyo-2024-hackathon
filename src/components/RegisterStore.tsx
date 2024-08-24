40a2d18073c0bb934ba0dc5d4e0e3c638e6fc987
sheagrief <3a.mad1earth4@gmail.com>
2024-08-24 17:55:01 +0900

    🐛 Fix Add store

--- src/components/RegisterStore.tsx
+++ src/components/RegisterStore.tsx
@@ -1,48 +1,52 @@
 import React, { useState } from "react";
 
 import { addStore } from "@/utils/store/management";
+import { TransactionButton, useActiveAccount } from "thirdweb/react";
 
 function RegisterStorePopup() {
   const [showPopup, setShowPopup] = useState(false);
   const [storeName, setStoreName] = useState("");
   const [walletAddress, setWalletAddress] = useState("");
 
-  const handleRegisterStore = (storeName: string) => () => {
-    addStore(storeName);
-    setShowPopup(false);
-  };
+  const activeAccount = useActiveAccount();
 
   return (
     <div>
       <button
-        onClick={() => setShowPopup(true)}
+        onClick={() => {
+          setShowPopup(true);
+          console.log("address", activeAccount?.address);
+        }}
         className="px-4 py-2 rounded bg-blue-500 text-white"
       >
-        Register Store
+        ストア登録
       </button>
 
       {showPopup && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded shadow-lg">
-            <h2 className="font-bold mb-4 text-black">Register User</h2>
+            <h2 className="font-bold mb-4 text-black">ストア登録</h2>
 
             <div className="mb-4">
-              <label className="block mb-2 text-black">User Name</label>
+              <label className="block mb-2 text-black">ストア名</label>
               <input
                 type="text"
                 value={storeName}
                 onChange={(e) => setStoreName(e.target.value)}
-                className="w-full px-3 py-2 border rounded"
+                className="w-full px-3 py-2 border rounded text-black"
               />
             </div>
 
             <div className="mb-4">
-              <label className="block mb-2 text-black">Wallet Address</label>
+              <label className="block mb-2 text-black">
+                ウォレットアドレス
+              </label>
               <input
                 type="text"
-                value={walletAddress}
+                value={activeAccount?.address || "ウォレット接続をしてください"}
+                disabled
                 onChange={(e) => setWalletAddress(e.target.value)}
-                className="w-full px-3 py-2 border rounded"
+                className="w-full px-3 py-2 border rounded text-gray-500"
               />
             </div>
 
@@ -53,12 +57,26 @@ function RegisterStorePopup() {
               >
                 Cancel
               </button>
-              <button
-                onClick={handleRegisterStore("DummyStoreName")}
+              <TransactionButton
+                unstyled
                 className="px-4 py-2 rounded bg-green-500 text-white"
+                transaction={() => {
+                  const tx = addStore(storeName);
+                  return tx;
+                }}
+                onTransactionSent={(result) => {
+                  console.log("Transaction submitted", result.transactionHash);
+                }}
+                onTransactionConfirmed={(receipt) => {
+                  console.log("Transaction confirmed", receipt.transactionHash);
+                  window.location.reload();
+                }}
+                onError={(error) => {
+                  console.error("Transaction error", error);
+                }}
               >
                 OK
-              </button>
+              </TransactionButton>
             </div>
           </div>
         </div>
