efc4ee1d2bf0878fddb937ce3d90970cb6648692
taskooh <taskooh@github.com>
2024-08-24 23:22:35 +0900

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { ThirdwebProvider } from "thirdweb/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "thirdweb SDK + Next starter",
+  title: "OpenBooking",
   description:
-    "Starter template for using thirdweb SDK with Next.js App router",
+    "A decentralized reservation system",
 };
 
 export default function RootLayout({
